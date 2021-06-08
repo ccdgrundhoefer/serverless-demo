4da215206cea5695eb05d3da4ea775cb3104f75a
Daniel Grundhöfer <daniel.grundhoefer@computacenter.com>
2021-06-08 11:46:37 +0200

    added insert value

--- backend/create/src/app.py
+++ backend/create/src/app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
 import psycopg2
 
 app = Flask(__name__)
@@ -9,6 +9,59 @@ app = Flask(__name__)
 def hello_world():
    target = os.environ.get('TARGET', 'World')
    return 'Hello {}!\n'.format(target)
+
+@app.route('/create', methods=['GET'])
+def create_value():
+   error = None
+
+   if request.method == 'GET':
+      if request.args.get('temp', ''):
+
+         db = os.environ.get('DB')
+         dbuser = os.environ.get('DBUSER')
+         dbpassword = os.environ.get('DBPASS')
+         dbhost = os.environ.get('DBHOST')
+
+         temperature = request.args.get('temp', '')
+         try:
+            temp = float(temperature)
+
+            db = os.environ.get('DB')
+            dbuser = os.environ.get('DBUSER')
+            dbpassword = os.environ.get('DBPASS')
+            dbhost = os.environ.get('DBHOST')
+
+            commands = """INSERT INTO temperature VALUES(%f.2) RETURNING id"""
+            
+            try:
+               # connect to the PostgreSQL server
+               conn = psycopg2.connect(
+                  host = dbhost,
+                  database = db,
+                  user = dbuser,
+                  password = dbpassword
+               )
+
+               cur = conn.cursor()
+
+               cur.execute(commands, (temperature,))
+               id = cur.fetchone()[0]
+
+               # commit the changes
+               conn.commit()
+               # close communication with the PostgreSQL database server
+               cur.close()
+
+
+               return "Database initialized"
+            except (Exception, psycopg2.DatabaseError) as error:
+               return error
+            finally:
+               if conn is not None:
+                  conn.close()
+         except ValueError:
+            return "Invalid temperature"
+
 
 @app.route('/initdb')
 def create_tables():
@@ -23,7 +76,7 @@ def create_tables():
       CREATE TABLE temperature (
          id SERIAL PRIMARY KEY,
          value decimal(255) NOT NULL
-         datetime timestamp
+         datetime TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
       )
       """)
    
@@ -35,17 +88,20 @@ def create_tables():
          user = dbuser,
          password = dbpassword
       )
-      
+
       cur = conn.cursor()
       # create table one by one
       for command in commands:
          cur.execute(command)
-      # close communication with the PostgreSQL database server
-      cur.close()
+      
       # commit the changes
       conn.commit()
+      # close communication with the PostgreSQL database server
+      cur.close()
+
+      return "Database initialized"
    except (Exception, psycopg2.DatabaseError) as error:
-      print(error)
+      return error
    finally:
       if conn is not None:
          conn.close()
