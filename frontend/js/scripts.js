a2339346d51362181eb3063f0260deeef2b8ea38
Daniel Grundhöfer <daniel.grundhoefer@computacenter.com>
2021-06-09 10:40:21 +0200

    frontend fixes

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,10 +1,15 @@
 $("#initdb").get("http://api-create-serverless-demo.apps.cluster-598a.598a.sandbox502.opentlc.com/initdb");
 $("#refresh").click(function() {
     $.get("http://api-read-serverless-demo.apps.cluster-598a.598a.sandbox502.opentlc.com", function (data) {
-        $("#temp").html(data)
+        
+        d = $.parseJSON(data);
+        
+        $("#temp").html(d.temp + "°C");
+        $("#humidity").html(d.humidity + "%");
 
         var currentdate = new Date();
 
-        $("#tempfooter").html(currentdate.getHours() + ":" + currentdate.getMinutes + ":" + currentdate.getSeconds())
+        $("#tempfooter").html(currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds());
+        $("#humfooter").html(currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds());
     });
 });
